d9f1a370129c61a8b45882ffda47e1d810c00c2a
rkrajancic <krajanci@ualberta.ca>
2023-10-04 19:18:42 -0600

    added shape :))

---
+++ app/src/main/java/com/example/cmput301f23t14/Shape.java
@@ -0,0 +1,27 @@
+package com.example.cmput301f23t14;
+
+public abstract class Shape {
+    private Integer x;
+    private Integer y;
+
+    Shape(Integer x, Integer y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+}
